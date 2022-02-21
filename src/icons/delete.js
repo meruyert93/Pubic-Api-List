5dd38ba03bab1b2779c6fa81b2f61ea81718123f
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2022-02-21 21:07:41 +0100

    add new icons

---
+++ src/icons/delete.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'delete': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 24 24',
+    data: '<circle pid="0" cx="12" cy="12" r="11.25" _fill="none" _stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><path pid="1" _fill="none" _stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.045 19.955l15.91-15.91"/>'
+  }
+})
---
+++ src/icons/search.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'search': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 24 24',
+    data: '<circle pid="0" cx="9.813" cy="9.812" r="9.063" transform="rotate(-23.025 9.813 9.812)" _fill="none" _stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><path pid="1" _fill="none" _stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.221 16.22l7.029 7.03"/>'
+  }
+})
