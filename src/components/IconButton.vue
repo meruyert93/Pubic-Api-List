3e3b5650b8e36c1d7f564499d1239704c8937580
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2022-02-22 15:18:55 +0100

    fix: rename title and remove extra code

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,19 +1,8 @@
 <template>
   <b-button
     class="iconButton"
-    :iconName="iconName"
-    :iconFill="iconFill"
-    :iconSize="iconSize"
-    :iconColor="iconColor"
     :disabled="disabled"
     :type="type"
-    :href="href"
-    :rel="rel"
-    :target="target"
-    :to="to"
-    :append="append"
-    :replace="replace"
-    :exact="exact"
     :tabindex="tabindex"
     v-on="$listeners"
   >
@@ -36,17 +25,7 @@ export default {
     'b-button': BButton
   },
   props: {
-    /**
-     * <router-link> prop: Setting append prop always appends the relative path to the current path
-     */
-    append: { type: Boolean },
     disabled: { type: Boolean, default: false },
-    /**
-     * <router-link> prop: The default active class matching behavior is inclusive match.
-     * Setting this prop forces the mode to exactly match the route
-     */
-    exact: { type: Boolean, default: false },
-    href: { type: String },
     /**
      * Defaults to `inherit` if left empty
      */
@@ -54,21 +33,7 @@ export default {
     iconFill: { type: Boolean, default: false },
     iconName: { type: String },
     iconSize: { type: Number },
-    rel: { type: String },
-    /**
-     * <router-link> prop: Setting the replace prop will call 'router.replace()'
-     * instead of 'router.push()'
-     * when clicked, so the navigation will not leave a history record
-     */
-    replace: { type: Boolean, default: false },
     tabindex: { type: Number },
-    target: { type: String, default: '_self' },
-    /**
-     * <router-link> prop: Denotes the target route of the link.
-     * When clicked, the value of the to prop will be passed to router.push() internally,
-     * so the value can be either a string or a Location descriptor object
-     */
-    to: { type: [String, Object] },
     type: { type: String, default: 'button' }
   }
 }
